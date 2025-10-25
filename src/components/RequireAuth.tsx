81fcb35e445a06b1d88380bd4a01a14d8824152d
Martin Irrgeher <martin.irrgeher@google.com>
2025-10-25 20:51:24 +0200

    Update

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,6 +1,12 @@
 
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '@/store/AuthContext';
+import { ReactElement } from "react";
+
+type Props = {
+  children: ReactElement;
+  modules?: string[];
+};
 
 export default function RequireAuth({ children }: { children: JSX.Element }) {
   const { token, user } = useAuth();
